7ae427ad901a79615a7238215b046be10e1db173
JaredCoto0418 <jaredcoto0418@gmail.com>
2025-10-10 14:04:42 -0300

    actualizado

--- GestionPedidos.Designer.cs
+++ GestionPedidos.Designer.cs
@@ -1,16 +1,18 @@
-﻿namespace InventarioAppTeoria
+﻿namespace GestionPedidos
 {
     partial class frmGestionPedidos
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ListBox lstPedidosEspera;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnDespachar;
+        private System.Windows.Forms.Label lblSiguiente;
+        private System.Windows.Forms.Label lblContador;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblTituloLista;
+        private System.Windows.Forms.Panel panelSiguiente;
+        private System.Windows.Forms.Label lblTituloSiguiente;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,42 +22,150 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.panelSiguiente = new System.Windows.Forms.Panel();
+            this.lblTituloSiguiente = new System.Windows.Forms.Label();
+            this.lblSiguiente = new System.Windows.Forms.Label();
+            this.lblContador = new System.Windows.Forms.Label();
+            this.lblTituloLista = new System.Windows.Forms.Label();
+            this.lstPedidosEspera = new System.Windows.Forms.ListBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnDespachar = new System.Windows.Forms.Button();
+            this.panelSiguiente.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // lblTitulo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(333, 194);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "HOLA HICE UN CAMBIO";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.ForeColor = System.Drawing.Color.DarkBlue;
+            this.lblTitulo.Location = new System.Drawing.Point(22, 25);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(630, 44);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "GESTIÓN DE PEDIDOS";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelSiguiente
+            // 
+            this.panelSiguiente.BackColor = System.Drawing.Color.LightYellow;
+            this.panelSiguiente.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelSiguiente.Controls.Add(this.lblTituloSiguiente);
+            this.panelSiguiente.Controls.Add(this.lblSiguiente);
+            this.panelSiguiente.Location = new System.Drawing.Point(22, 88);
+            this.panelSiguiente.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panelSiguiente.Name = "panelSiguiente";
+            this.panelSiguiente.Size = new System.Drawing.Size(630, 100);
+            this.panelSiguiente.TabIndex = 1;
+            // 
+            // lblTituloSiguiente
+            // 
+            this.lblTituloSiguiente.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.lblTituloSiguiente.ForeColor = System.Drawing.Color.DarkOrange;
+            this.lblTituloSiguiente.Location = new System.Drawing.Point(11, 12);
+            this.lblTituloSiguiente.Name = "lblTituloSiguiente";
+            this.lblTituloSiguiente.Size = new System.Drawing.Size(608, 31);
+            this.lblTituloSiguiente.TabIndex = 0;
+            this.lblTituloSiguiente.Text = "🎯 SIGUIENTE EN LA COLA:";
+            // 
+            // lblSiguiente
+            // 
+            this.lblSiguiente.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.lblSiguiente.ForeColor = System.Drawing.Color.Gray;
+            this.lblSiguiente.Location = new System.Drawing.Point(11, 44);
+            this.lblSiguiente.Name = "lblSiguiente";
+            this.lblSiguiente.Size = new System.Drawing.Size(608, 38);
+            this.lblSiguiente.TabIndex = 1;
+            this.lblSiguiente.Text = "No hay pedidos en espera";
+            // 
+            // lblContador
+            // 
+            this.lblContador.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.lblContador.ForeColor = System.Drawing.Color.DarkBlue;
+            this.lblContador.Location = new System.Drawing.Point(22, 206);
+            this.lblContador.Name = "lblContador";
+            this.lblContador.Size = new System.Drawing.Size(225, 31);
+            this.lblContador.TabIndex = 2;
+            this.lblContador.Text = "Total en cola: 0";
+            // 
+            // lblTituloLista
+            // 
+            this.lblTituloLista.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.lblTituloLista.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblTituloLista.Location = new System.Drawing.Point(22, 244);
+            this.lblTituloLista.Name = "lblTituloLista";
+            this.lblTituloLista.Size = new System.Drawing.Size(225, 31);
+            this.lblTituloLista.TabIndex = 3;
+            this.lblTituloLista.Text = "📋 Pedidos en Espera:";
+            // 
+            // lstPedidosEspera
+            // 
+            this.lstPedidosEspera.BackColor = System.Drawing.Color.White;
+            this.lstPedidosEspera.Font = new System.Drawing.Font("Consolas", 10F);
+            this.lstPedidosEspera.FormattingEnabled = true;
+            this.lstPedidosEspera.ItemHeight = 23;
+            this.lstPedidosEspera.Location = new System.Drawing.Point(22, 281);
+            this.lstPedidosEspera.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.lstPedidosEspera.Name = "lstPedidosEspera";
+            this.lstPedidosEspera.Size = new System.Drawing.Size(630, 165);
+            this.lstPedidosEspera.TabIndex = 4;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnAgregar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregar.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.btnAgregar.ForeColor = System.Drawing.Color.White;
+            this.btnAgregar.Location = new System.Drawing.Point(22, 488);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(304, 62);
+            this.btnAgregar.TabIndex = 5;
+            this.btnAgregar.Text = "➕ Agregar Pedido";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnDespachar
+            // 
+            this.btnDespachar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(125)))), ((int)(((byte)(50)))));
+            this.btnDespachar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDespachar.Enabled = false;
+            this.btnDespachar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDespachar.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.btnDespachar.ForeColor = System.Drawing.Color.White;
+            this.btnDespachar.Location = new System.Drawing.Point(349, 488);
+            this.btnDespachar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnDespachar.Name = "btnDespachar";
+            this.btnDespachar.Size = new System.Drawing.Size(304, 62);
+            this.btnDespachar.TabIndex = 6;
+            this.btnDespachar.Text = "🚀 Despachar Siguiente";
+            this.btnDespachar.UseVisualStyleBackColor = false;
+            this.btnDespachar.Click += new System.EventHandler(this.btnDespachar_Click);
             // 
             // frmGestionPedidos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1251, 625);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.panelSiguiente);
+            this.Controls.Add(this.lblContador);
+            this.Controls.Add(this.lblTituloLista);
+            this.Controls.Add(this.lstPedidosEspera);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnDespachar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
             this.Name = "frmGestionPedidos";
-            this.Text = "Gestion De Pedidos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestión de Pedidos - Sistema de Cola";
+            this.panelSiguiente.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
-
-        #endregion
-
-        private System.Windows.Forms.Label label1;
     }
 }
--- GestionPedidos.cs
+++ GestionPedidos.cs
@@ -1,25 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace InventarioAppTeoria
+namespace GestionPedidos
 {
     public partial class frmGestionPedidos : Form
     {
+        private Queue<string> colaPedidos;
+        private int contadorPedidos;
+
         public frmGestionPedidos()
         {
             InitializeComponent();
+            colaPedidos = new Queue<string>();
+            contadorPedidos = 0;
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void btnAgregar_Click(object sender, EventArgs e)
         {
+            // Solicitar el nombre del pedido al usuario
+            string nombrePedido = Microsoft.VisualBasic.Interaction.InputBox(
+                "Ingrese la descripción del pedido:",
+                "Nuevo Pedido",
+                "");
 
+            if (!string.IsNullOrWhiteSpace(nombrePedido))
+            {
+                contadorPedidos++;
+                string pedido = $"Pedido #{contadorPedidos} - {nombrePedido}";
+
+                // Agregar a la cola
+                colaPedidos.Enqueue(pedido);
+
+                // Agregar a la lista visual
+                lstPedidosEspera.Items.Add(pedido);
+
+                // Actualizar interfaz
+                ActualizarSiguiente();
+
+                MessageBox.Show($"Pedido agregado exitosamente!\nTotal en cola: {colaPedidos.Count}",
+                              "Éxito",
+                              MessageBoxButtons.OK,
+                              MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnDespachar_Click(object sender, EventArgs e)
+        {
+            if (colaPedidos.Count > 0)
+            {
+                // Despachar el siguiente pedido (FIFO)
+                string pedidoDespachado = colaPedidos.Dequeue();
+
+                // Remover de la lista visual
+                lstPedidosEspera.Items.RemoveAt(0);
+
+                // Actualizar interfaz
+                ActualizarSiguiente();
+
+                MessageBox.Show($"Pedido despachado:\n\n{pedidoDespachado}\n\nPedidos restantes: {colaPedidos.Count}",
+                              "Pedido Despachado",
+                              MessageBoxButtons.OK,
+                              MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No hay pedidos en espera para despachar.",
+                              "Advertencia",
+                              MessageBoxButtons.OK,
+                              MessageBoxIcon.Warning);
+            }
+        }
+
+        private void ActualizarSiguiente()
+        {
+            if (colaPedidos.Count > 0)
+            {
+                // Mostrar el siguiente sin sacarlo de la cola (Peek)
+                string siguiente = colaPedidos.Peek();
+                lblSiguiente.Text = $"Siguiente: {siguiente}";
+                lblSiguiente.ForeColor = System.Drawing.Color.DarkGreen;
+                btnDespachar.Enabled = true;
+            }
+            else
+            {
+                lblSiguiente.Text = "No hay pedidos en espera";
+                lblSiguiente.ForeColor = System.Drawing.Color.Gray;
+                btnDespachar.Enabled = false;
+            }
+
+            // Actualizar contador total
+            lblContador.Text = $"Total en cola: {colaPedidos.Count}";
         }
     }
 }
